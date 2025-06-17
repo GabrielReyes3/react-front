ec0e2a275688e93837f6ad9d0dd97c21c593ba72
GabrielReyes3 <119997410+GabrielReyes3@users.noreply.github.com>
2025-06-16 20:48:18 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Gabriel Reyes
+          Gabriel Reyes - gamoe
         </p>
         <a
           className="App-link"
